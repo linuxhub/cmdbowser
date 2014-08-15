1d677bac481a3e19514988d90a4fc66a6e6e0e4a
liyang24 <liyang24@meituan.com>
2014-08-15 10:47:07 +0800

    命令行浏览器功能基本完善

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 # coding=utf-8
 
+
 import sys
 import os
 import socket
 import re
 
+
 def getStrVal(s):
     return s.replace('\r', '') if s else ''
+
+
+def showLine():
+    print '------------------------------------------------------------'
+
 
 def parseURL(url):
     req = {}
@@ -30,6 +37,7 @@ def parseURL(url):
 
     return req
 
+
 def parseResponseHeaders(data):
     spl = re.split("\r\n(.+:\ .+)\r\n", data)
     headers = {}
@@ -42,29 +50,37 @@ def parseResponseHeaders(data):
         return headers
     return False
 
+
 def getStatusCode(data):
     res = re.match("^HTTP/1.1\ (\d+)\ ", data)
     if res:
         return res.groups()[0]
     return False
 
+
 def displayDict(d):
     if d:
         for (key,value) in d.items():
             print '%s: %s' % (key, value)
 
+
 def app(req):
     try:
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print '成功创建socket... '
     except socket.error, e:
-         print 'Socket error`` %s' % e
+        print '创建socket失败，退出程序... %s' % e
+        exit()
 
     try:
         soc.connect((req['IP'], req['PORT']))
+        print '成功连接到远程服务器：http://%s:%d' % (req['IP'], req['PORT'])
     except socket.error, e:
-        print 'Connect error %s' % e
+        print '无法连接至远程服务器，退出程序...\nERROR: %s' % e
         soc.close()
+        exit()
 
+    print '正在解析响应的头信息数据....'
     try:
         s = 'GET ' + req['PATH'] + ' HTTP/1.1\r\n'
         s += 'Host: ' + req['HOST'] + '\r\n'
@@ -83,23 +99,25 @@ def app(req):
 
     data = soc.recv(1024)
     headers = parseResponseHeaders(data)
-    print headers
-    displayDict(headers)
+
     if not headers:
         print '远程WEB服务器响应格式错误...exit!'
         soc.close()
         exit()
     sc = int(headers["Status-Code"])
     if sc >= 300 and sc < 400:
+        showLine()
+        displayDict(headers)
+        showLine()
         print "完成请求，需重定向至%s" % headers["Location"]
         return headers["Location"]
 
+    s = data.find('\r\n\r\n', 0, len(data)) + 4
     # Content-Length
     if 'Content-Length' in headers.keys():
-        html_idx = data.find('\r\n\r\n', 0, len(data)) + 4
-        data_len = long(headers['Content-Length']) + long(len(data[0: html_idx]))
+        e = long(headers['Content-Length']) + long(len(data[0: s]))
         while True:
-            if len(data) >= data_len:
+            if len(data) >= e:
                 break
             data += soc.recv(1024)
     # Transfer-Encoding
@@ -109,8 +127,16 @@ def app(req):
             if len(recv) == 0:
                 break
             data += recv
-    print data
+        e = data.find('\r\n0\r\n', 0, len(data))
+    html = data[s: e]
     soc.close()
+
+    showLine()
+    raw_input('恭喜，操作成功！按Enter键查看头信息...')
+    displayDict(headers)
+    showLine()
+    raw_input('继续按Enter键查看下载的网页数据...')
+    print html
 
     return False
 
@@ -121,6 +147,8 @@ if __name__=='__main__':
         print "无域名，命令Demo：python app.py meituan.com"
         exit()
 
+    os.system('clear')
+    showLine()
     # 限制重定向的次数最多为5次
     count = 5
     req = parseURL(sys.argv[1])
@@ -128,14 +156,14 @@ if __name__=='__main__':
         reURL = app(req)
         if reURL and count:
             count -= 1
-            raw_input('敲击任何键，进行重定向操作...')
+            raw_input('敲Enter键，进行重定向操作...')
             os.system('clear')
             req = parseURL(reURL)
-            print req
         else:
             break
 
-    print 'Done!'
+    showLine()
+    raw_input('Done! 完成操作...按Enter键退出程序...')
 
 
 
